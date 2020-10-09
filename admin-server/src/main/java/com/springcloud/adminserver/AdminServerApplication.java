600eaa4d11d7c1af3d611e768a69710456537694
YUDI-Corgi <1019509861@qq.com>
2020-10-09 23:08:31 +0800

    add springboot-admin monitoring service

---
+++ admin-server/src/main/java/com/springcloud/adminserver/AdminServerApplication.java
@@ -0,0 +1,18 @@
+package com.springcloud.adminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+// 启用 SpringBoot-admin 服务
+@EnableAdminServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
