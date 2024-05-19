0366cfdbbe6733c70dcf91dd3e96e03ceb4b3249
YUDI-Corgi <1019509861@qq.com>
2024-05-19 22:22:22 +0800

    style: comment.

--- cloud-stream/stream-consumer/src/main/java/org/springcloud/stream/handler/PartitionedMQReceiver.java
+++ cloud-stream/stream-consumer/src/main/java/org/springcloud/stream/handler/PartitionedMQReceiver.java
@@ -1,7 +1,10 @@
 package org.springcloud.stream.handler;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
 
 /**
  * 分区消费者
@@ -14,20 +17,16 @@ public class PartitionedMQReceiver {
     public Consumer<Message<String>> consumerOne() {
         return msg -> {
             System.out.println("消息分区一：" + msg.getPayload());
-            msg.getHeaders().forEach((k, v) -> {
-                System.out.println(k + ":" + v);
-            }
-        }
+            msg.getHeaders().forEach((k, v) -> System.out.println(k + ":" + v));
+        };
     }
 
     @Bean
     public Consumer<Message<String>> consumerTwo() {
         return msg -> {
             System.out.println("消息分区二：" + msg.getPayload());
-            msg.getHeaders().forEach((k, v) -> {
-                System.out.println(k + ":" + v);
-            }
-        }
+            msg.getHeaders().forEach((k, v) -> System.out.println(k + ":" + v));
+        };
     }
 
 }
